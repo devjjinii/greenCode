10f68750a3c00b993cf37c46dc27c83358a720b4
황윤지 <yunji.hwang@balaan.co.kr>
2020-12-06 12:20:20 +0900

    Prototype

--- 공유파일/17.prototype.js
+++
@@ -1,8 +0,0 @@
-/**
-* 준비할 내용
-* .prototype
-* __proto__
-* [[Prototype]]
-* property
-* https://medium.com/@pks2974/javascript-%EC%99%80-prototype-%ED%94%84%EB%A1%9C%ED%86%A0-%ED%83%80%EC%9E%85-515f759bff79
-*/
